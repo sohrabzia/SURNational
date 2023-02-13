d6067b090ecbcd7497df13054b22edf91333ee6d
Sohrab zia <ziasohrab@gmail.com>
2023-02-13 16:00:18 +0300

    Add files via upload

---
+++ surNationalTheme/js/custom.js
@@ -0,0 +1,64 @@
+$('#featureProducts').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+   autoplayTimeout:2000,
+    responsiveClass:true,
+    navText : ["<i class='fa-sharp fa-solid fa-circle-chevron-left'></i>","<i class='fa-sharp fa-solid fa-circle-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:true
+        }
+    }
+})
+
+$('#ourBrands').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    navText : ["<i class='fa-sharp fa-solid fa-circle-chevron-left'></i>","<i class='fa-sharp fa-solid fa-circle-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:2,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+
+
+});
+
+$('.moreless-button').click(function() {
+    $('.moretext').slideToggle();
+    if ($('.moreless-button').text() == "Read more") {
+      $(this).text("See less")
+    } else {
+      $(this).text("See more")
+    }
+  });
+
+
+ 
+  
+
+  //read more
+  
